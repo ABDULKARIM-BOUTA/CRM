56c78a7298e861b1709671d2a38a0862336993ec
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-02-27 22:40:43 +0300

    implementing models and user configuration

---
+++ crm/leads/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from leads.models import Agent, Client
+
+# Register your models here.
+admin.site.register(Agent)
+admin.site.register(Client)
---
+++ crm/leads/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+from accounts.models import User
+
+# Create your models here.
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.user.email
+
+class Client(models.Model): #this class stores the records of clients
+    source_options = (
+        ('1', 'Internet'),
+        ('2', 'Newsletter'),
+        ('3', 'TV')
+    )
+
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    source = models.CharField(max_length=50, choices=source_options)
+    profile_picture = models.ImageField(blank=True, null=True)
+    phoned = models.BooleanField(default=False)
+
+    age = models.IntegerField(validators=[MinValueValidator(18), MaxValueValidator(120)])
+    files = models.FileField(blank=True, null=True)
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
