ea1975d9c0da1817138f3e9ad1d6ac854de39167
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-03-06 14:59:32 +0300

    adding the previous functionality to the other classes

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import reverse
 from django.core.mail import send_mail
-from leads.models import Agent, Client
+from leads.models import Client
 from leads.forms import ClientForm
 from django.views.generic import TemplateView, ListView, DetailView, DeleteView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -8,11 +8,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class LeadDeleteView(LoginRequiredMixin, DeleteView):
-    queryset = Client.objects.all()
     template_name = 'lead/lead_delete.html'
 
     def get_success_url(self):
         return reverse('leads:lead-list')
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # organization only see their clients
+        if user.is_organizor:
+            queryset = Client.objects.filter(organization__user=user)
+
+        # Agents only see their clients
+        elif user.is_agent:
+            queryset = Client.objects.filter(agent__user=user)
+
+        return queryset
 
 class LeadUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'lead/lead_update.html'
@@ -21,6 +33,19 @@ class LeadUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse('leads:lead-detail', args=[self.object.pk])
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # organization only see their clients
+        if user.is_organizor:
+            queryset = Client.objects.filter(organization__user=user)
+
+        # Agents only see their clients
+        elif user.is_agent:
+            queryset = Client.objects.filter(agent__user=user)
+
+        return queryset
 
 class LeadCreateView(LoginRequiredMixin, CreateView):
     template_name = 'lead/lead_form.html'
@@ -42,17 +67,33 @@ class LeadListView(LoginRequiredMixin, ListView):
     template_name = 'lead/lead_list.html'
 
     def get_queryset(self):
-        if self.request.user.is_organizor:
-            queryset = Client.objects.filter(organization__user=self.request.user)
+        user = self.request.user
 
-        if self.request.user.is_agent:
-            queryset = Client.objects.filter(agent__user=self.request.user)
+        # organization only see their clients
+        if user.is_organizor:
+            queryset = Client.objects.filter(organization__user=user)
+
+        # Agents only see their clients
+        elif user.is_agent:
+            queryset = Client.objects.filter(agent__user=user)
 
         return queryset
 
 class LeadDetailView(LoginRequiredMixin, DetailView):
     template_name = 'lead/lead_detail.html'
-    queryset = Client.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # organization only see their clients
+        if user.is_organizor:
+            queryset = Client.objects.filter(organization__user=user)
+
+        # Agents only see their clients
+        elif user.is_agent:
+            queryset = Client.objects.filter(agent__user=user)
+
+        return queryset
 
 class LandingPageView(TemplateView):
     template_name = 'partials/landing_page.html'
