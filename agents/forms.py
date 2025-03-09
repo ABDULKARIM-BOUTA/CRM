d63693dbfca4109e4e039f2991e872413966e626
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-03-09 20:55:33 +0300

    test

--- agents/forms.py
+++ agents/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from accounts.models import User
+from agents.models import Agent
+from leads.models import Client
 
 class AgentForm(forms.ModelForm):
     class Meta:
@@ -9,5 +11,19 @@ class AgentForm(forms.ModelForm):
             'username',
             'first_name',
             'last_name',
-
         ]
+
+class AgentAssignForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['agent']
+
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+
+    # To override the agent field so organizations can assign their agents only to a client
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop('request')
+        organization = request.user.organization
+        agents = Agent.objects.filter(organization=organization)
+        super(AgentAssignForm, self).__init__(*args, **kwargs)
+        self.fields['agent'].queryset = agents
--- agents/views.py
+++ agents/views.py
@@ -1,10 +1,11 @@
 from random import randint
 from django.shortcuts import reverse
-from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
-from agents.forms import AgentForm
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView, FormView
+from agents.forms import AgentForm, AgentAssignForm
 from agents.mixins import LoginAndOrganizorRequiredMixin
 from agents.models import Agent
 from django.core.mail import send_mail
+from leads.models import Client
 
 # Create your views here.
 
@@ -76,3 +77,30 @@ class AgentUpdateView(LoginAndOrganizorRequiredMixin, UpdateView):
     def get_queryset(self):
         organization = self.request.user.organization
         return Agent.objects.filter(organization=organization)
+
+class AgentAssignView(LoginAndOrganizorRequiredMixin, UpdateView):
+    template_name = 'agent/agent_assign.html'
+    form_class = AgentAssignForm
+    queryset = Client.objects.all()
+
+    # it helps to override the agent field in the AgentAssignForm
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super(AgentAssignView, self).get_form_kwargs(**kwargs)
+        kwargs.update({'request': self.request})
+        return kwargs
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # organization only see their clients
+        if user.is_organizor:
+            queryset = Client.objects.filter(organization__user=user)
+
+        # Agents only see their clients
+        elif user.is_agent:
+            queryset = Client.objects.filter(agent__user=user)
+
+        return queryset
+
+    def get_success_url(self):
+        return reverse('leads:lead-detail', args=[self.object.pk])
