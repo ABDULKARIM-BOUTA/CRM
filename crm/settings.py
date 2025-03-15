407b790934fe06c154a5424ab8e5b2a4dd1eeffb
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-03-15 17:08:09 +0300

    adding argon2 password hasher

--- crm/settings.py
+++ crm/settings.py
@@ -156,3 +156,10 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.Argon2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
