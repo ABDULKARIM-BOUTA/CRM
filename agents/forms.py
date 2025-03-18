4b657839037410d2ffaa3608207df12c6bbc9220
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-03-19 00:40:07 +0300

    limiting username length to 15

--- agents/forms.py
+++ agents/forms.py
@@ -2,16 +2,49 @@ from django import forms
 from accounts.models import User
 from agents.models import Agent
 from clients.models import Client
+from django.core.exceptions import ValidationError
 
 class AgentForm(forms.ModelForm):
+    email = forms.EmailField()
+    username = forms.CharField(max_length=150)
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=150, required=False)
+
     class Meta:
-        model = User
-        fields =  [
-            'email',
-            'username',
-            'first_name',
-            'last_name',
-        ]
+        model = Agent
+        exclude = ['user','organization']  # Add other fields specific to the Agent model
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.user:
+            self.fields['email'].initial = self.instance.user.email
+            self.fields['username'].initial = self.instance.user.username
+            self.fields['first_name'].initial = self.instance.user.first_name
+            self.fields['last_name'].initial = self.instance.user.last_name
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise ValidationError("Email is required.")
+
+        # Check if the email is already in use by another user
+        user_with_email = User.objects.filter(email=email).exclude(pk=self.instance.user.pk).first()
+        if user_with_email:
+            raise ValidationError("This email is already in use by another user.")
+
+        return email
+
+    def save(self, commit=True):
+        agent = super().save(commit=False)
+        if commit:
+            agent.save()
+            user = agent.user
+            user.email = self.cleaned_data['email']
+            user.username = self.cleaned_data['username']
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.save()
+        return agent
 
 class AgentAssignForm(forms.ModelForm):
     class Meta:
